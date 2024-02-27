6c100f083a558067396c7d9a8440a79af500c7d9
Stefan Com <stefancomandnt@gmail.com>
2024-02-27 16:46:32 +0200

    Added connection to db and fiber server startup in main.go

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"go-chat-app/database"
+	"log"
+	"os"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load .env vars
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error while loading env: \n%v\n", err)
+	}
+
+	// Connect to postgresql database
+	database.DB, err = database.InitDb(database.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		SSLMode:  os.Getenv("SSLMODE"),
+		DBName:   os.Getenv("DB_NAME"),
+	})
+
+	if err != nil {
+		log.Fatalf("Error while opening db: \n%v\n", err)
+	}
+
+	router := fiber.New()
+
+	// Start server
+	err = router.Listen(":7000")
+	if err != nil {
+		log.Fatalf("Error while starting server: \n%v\n", err)
+	}
 }
