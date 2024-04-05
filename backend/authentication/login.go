0c0e47118427aea9889a5c216e02d1933735c1c6
Stefan Com <stefancomandnt@gmail.com>
2024-04-05 19:16:37 +0300

    Added email verification to login with db verification sessions

--- backend/authentication/login.go
+++ backend/authentication/login.go
@@ -32,18 +32,6 @@ func Login(ctx *fiber.Ctx) error {
 
 	var emailBody = fmt.Sprintf("<p>Here is your verification code, bitch</p><h1>%v</h1>", verificationCode)
 
-	go CodeTimeOut()
-	SendGoMail("stefancomandant@gmail.com", body.Email, "", emailBody)
-	emailCodeChannel <- verificationCode
-
-	verificationCodeStatus := <-emailCodeChannel
-	if verificationCodeStatus == "failure" {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{"status": "error", "response": "Invalid verification code!"})
-	}
-	if verificationCodeStatus == "timeout" {
-		return ctx.Status(fiber.StatusGatewayTimeout).JSON(&fiber.Map{"status": "timeout", "response": "Code verification timeout"})
-	}
-
 	var user User
 
 	err = database.DB.Table("users").Where("email = ?", body.Email).First(&user).Error
@@ -63,7 +51,9 @@ func Login(ctx *fiber.Ctx) error {
     return err
   }
 	
-	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{"status": "success", "response": "Succesfully logged in account!"})
+	SendGoMail("stefancomandant@gmail.com", body.Email, "", emailBody)
+
+  return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{"status": "success", "response": "Succesfully logged in account!", "id": user.ID})
 }
 
 func createSession(ctx *fiber.Ctx, userID int) error {
