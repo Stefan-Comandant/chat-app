5fa09beb9b2a810bfc0557b365836768b7abf4e1
Stefan Com <stefancomandnt@gmail.com>
2024-04-20 19:11:28 +0300

    Rewrote html for /register so it matches the /login page for email verifycation

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,45 +1,30 @@
 <script lang="ts">
-	import { VerifyWithCode, Register } from '$lib/authentication.ts';
+	import { goto } from '$app/navigation';
+	import { Register } from '$lib/authentication.ts';
 	import RegisterForm from '$lib/components/forms/Register-Form.svelte';
-	import type { VerificationSession, HTTPResponse } from '$lib/interfaces.ts';
-
-	let verification: VerificationSession = {
-		userid: 30
-	};
+	import type { HTTPResponse } from '$lib/interfaces.ts';
 
 	let response: HTTPResponse = {
 		response: ''
 	};
 </script>
 
-<div class="body">
-	<div class="container">
-		<RegisterForm
-			{response}
-			on:register={async (event) => {
-				response = await Register(event.detail);
+<div class="container">
+	<RegisterForm
+		{response}
+		on:register={async (event) => {
+			response = await Register(event.detail);
 
-				verification.userid = response.id;
-			}}
-		/>
-	</div>
-
-	<input type="text" bind:value={verification.code} />
-	<button
-		type="button"
-		on:click={async () => {
-			response = await VerifyWithCode(verification);
-		}}>Verify</button
-	>
+			goto(`/authorize/${response.id}`);
+		}}
+	/>
 </div>
 
 <style>
-	.body {
-		height: 100dvh;
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+	.container {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
 	}
 </style>
