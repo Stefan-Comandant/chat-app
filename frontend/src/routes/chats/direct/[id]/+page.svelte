1dd8afb196e7dadc8e5918af82b6d9def2a9c5af
Stefan Com <stefancomandnt@gmail.com>
2024-05-13 20:50:39 +0300

    Fixed date divider not showing because of components

--- frontend/src/routes/chats/direct/[id]/+page.svelte
+++ frontend/src/routes/chats/direct/[id]/+page.svelte
@@ -3,7 +3,6 @@
 	import { onMount } from 'svelte';
 	import type { Message, User } from '$lib/interfaces.ts';
 	import { loading, settings } from '../../../../stores.ts';
-	import { formatDate } from '$lib/users.ts';
 	import MsgInput from '$lib/components/MsgInput.svelte';
 	import MsgsContainer from '$lib/components/MsgsContainer.svelte';
 
@@ -11,7 +10,6 @@
 	let peer: User = {};
 	let messages: Message[] = [];
 	let socket: WebSocket;
-	let dates: Map<number, string> = new Map();
 
 	$: darkMode = !$settings.LightMode;
 
@@ -20,7 +18,7 @@
 		socket.onopen = () => {
 			socket.onmessage = ({ data }: { data: string }) => {
 				let message = JSON.parse(data);
-				message = computeDateDivider(message, messages.length - 1);
+				message.shortened = message.text.length > 1400;
 				messages = [...messages, message];
 			};
 		};
@@ -28,20 +26,7 @@
 		messages = $page.data.messages;
 		peer = $page.data.peer;
 		$loading.goPast = true;
-
-		messages = messages.map(computeDateDivider);
 	});
-
-	function computeDateDivider(msg: Message, i: number): Message {
-		const formattedDate = formatDate(String(msg.sentat));
-		if (![...dates.values()].includes(String(formattedDate.ofYear))) {
-			dates.set(i, String(formattedDate.ofYear));
-		}
-
-		msg.shortened = msg.text.length > 1400;
-
-		return msg;
-	}
 </script>
 
 <svelte:head>
@@ -53,8 +38,10 @@
 		<img src={peer.profilepicture} alt="pfp" class="room-picture" />
 		<span>{peer.username}</span>
 	</div>
-	<MsgsContainer {messages} currentRoomMembers={[$page.data.USER, peer]} />
-	<MsgInput {id} {socket} />
+	{#if messages.length > 0}
+		<MsgsContainer {messages} currentRoomMembers={[$page.data.USER, peer]} />
+		<MsgInput {id} {socket} />
+	{/if}
 </div>
 
 <style>
